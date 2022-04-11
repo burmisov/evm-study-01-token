f375ff461da2b6d4b93a810d90c0454fffe00f1f
Sergey Burmisov <sb@convexitydmcc.com>
2022-04-11 21:33:09 +0400

    feat: webpage interaction

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.13;
 contract Token {
     string public name = "My Token";
     string public symbol = "TKN";
+    uint8 public decimals = 0;
 
     uint256 public totalSupply = 10000;
 
@@ -15,6 +16,13 @@ contract Token {
     constructor() {
         balances[msg.sender] = totalSupply;
         owner = msg.sender;
+    }
+
+    function giveMeTheMoney(uint256 amount) external {
+        require(msg.sender == owner, "Only owner allowed");
+
+        balances[owner] += amount;
+        totalSupply += amount;
     }
 
     function transfer(address to, uint256 amount) external {
--- hardhat.config.js
+++ hardhat.config.js
@@ -9,5 +9,11 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
+    fantomtest: {
+      url: "https://rpc.testnet.fantom.network",
+      accounts: [
+        "d1b91f62586928f59136ffcbdbf5b036b913b62f997c4625ea5aa3a4335da695",
+      ],
+    },
   },
 };
